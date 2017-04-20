95c6e0fdcacd0519d9447856ce93e63a6d89c54d
Frank17 <zouzhipeng.1@163.com>
2017-04-20 17:26:58 +0800

    Update createUserTable.sql
    添加数据类型

--- src/main/resources/sqls/createUserTable.sql
+++ src/main/resources/sqls/createUserTable.sql
@@ -1,4 +1,4 @@
 create database dbchatroom;/*创建数据库*/
 
 --创建用户表
-create User(username CHAR(20) PRIMARY KEY, password NOT NULL, nickname VARCHAR(20), sex CHAR(2));
+create User(username CHAR(20) PRIMARY KEY, password NOT CHAR(20) NULL, nickname VARCHAR(20), sex CHAR(2));
