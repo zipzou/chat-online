7dde51d9c762c7ac3db9970eb7926b438177f6bd
邹智鹏 <zouzhipeng.1@163.com>
2017-04-21 10:52:58 +0800

    更新SQL文件

--- src/main/resources/sqls/createUserTable.sql
+++ src/main/resources/sqls/createUserTable.sql
@@ -1,4 +1,4 @@
 create database dbchatroom;/*创建数据库*/
 
 --创建用户表
-create table ChatUser(username CHAR(20) PRIMARY KEY, password CHAR(20) NOT NULL, nickname VARCHAR(20), sex CHAR(2));
+create table ChatUser(username CHAR(20) PRIMARY KEY, password CHAR(64) NOT NULL, nickname VARCHAR(20), sex CHAR(2));
