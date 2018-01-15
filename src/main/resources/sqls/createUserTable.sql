564390c703b8dba5b80ec013b1644c6d1df47664
Frank Tsau <zouzhipeng.1@163.com>
2018-01-15 13:58:34 +0800

    修改SQL增加视图创建

--- src/main/resources/sqls/createUserTable.sql
+++ src/main/resources/sqls/createUserTable.sql
@@ -2,3 +2,5 @@ create database dbchatroom;/*创建数据库*/
 
 --创建用户表
 create table ChatUser(username CHAR(20) PRIMARY KEY, password CHAR(64) NOT NULL, nickname VARCHAR(20), sex CHAR(2));
+
+create view ViewUserToChat as select username, password, nickname, sex from ChatUser;
