97b7e11f87b4c3ef47d8738083f2439780033473
Zip Zou <zipzou@Zips-MacBook-Pro.local>
2021-04-08 20:22:14 +0800

    删除原有项目结构

--- src/main/java/com/chatroom/interfaces/IJsonSeriserialize.java
+++
@@ -1,8 +0,0 @@
-package com.chatroom.interfaces;
-
-import org.json.JSONObject;
-
-public interface IJsonSeriserialize {
-	public JSONObject toJson();
-	public void readFromJson(JSONObject json);
-}
